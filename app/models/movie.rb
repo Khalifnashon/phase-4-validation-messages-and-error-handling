306911837927511a0b7bca4f121c505200a20169
+ <+>
2023-03-19 15:14:15 +0300

    completed code along and bonus

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
     CATEGORIES = ['Comedy', 'Drama', 'Animation', 'Mystery', 'Horror', 'Fantasy', 'Action', 'Documentary', 'Science Fiction']
   
-    validates :title, presence: true
+    validates :title, presence: true, uniqueness: true
     validates :year, numericality: {
       greater_than_or_equal_to: 1888,
       less_than_or_equal_to: Date.today.year
